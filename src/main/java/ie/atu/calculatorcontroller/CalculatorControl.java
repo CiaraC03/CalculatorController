b8723ea7459d4f7a5234a0cb7878d8e3726a6257
ciara <g00402357@atu.ie>
2023-10-02 22:01:50 +0100

    added getters and setters

--- src/main/java/ie/atu/calculatorcontroller/CalculatorControl.java
+++ src/main/java/ie/atu/calculatorcontroller/CalculatorControl.java
@@ -4,13 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class CalculatorControl {
 
 
     //create calculate to enter two numbers and operation to get request from website
     @GetMapping("/calculate")
-    public String calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation)
+    public Object calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation)
     {
         return "Operation" + operation + ", your total is ";
     }
--- src/main/java/ie/atu/calculatorcontroller/Operations.java
+++ src/main/java/ie/atu/calculatorcontroller/Operations.java
@@ -1,32 +1,54 @@
 package ie.atu.calculatorcontroller;
 
+import java.util.Scanner;
+
 public class Operations {
 
-    int total;
+    int num1, num2, total;
+    String operation;
+
+    public Operations(int num1, int num2, String operation)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.operation = operation;
+    }
+
+    public int getNum1(){
+        return num1;
+    }
+    public int getNum2(){
+        return num2;
+    }
+    public String getOperation(){
+        return operation;
+    }
+
+
 
     //addition function
-    public int add(int num1, int num2)
+    public int add()
     {
         total = num1+num2;
         return total;
     }
 
     //division function
-    public float divide(int num1, int num2)
+    public float divide()
     {
         total = num1/num2;
         return total;
     }
 
     //subtract function
-    public int subtract(int num1, int num2)
+    public int subtract()
     {
         total = num1 - num2;
         return total;
     }
 
     //multiply function
-    public int multiply(int num1, int num2)
+    public int multiply()
     {
         total = num1 * num2;
         return total;
